6f950568310f2ae963e92cee9418210fd21b8f94
Lara Bjork <lara.m.bjork@gmail.com>
2020-01-25 14:58:50 -0800

    tidy up product controller file, edit text and formatting on index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,8 @@
 class ProductsController < ApplicationController
 
-
-  # def index
-  #   @products = Product.all
-  #   render :index
-  # end
   def index
-  s = params[:sorter]
-    if s == "MostRev"
+    s = params[:sorter]
+    if s == "Buzzy"
       @products = Product.most_reviews
     elsif s== "Rwandan"
       @products = Product.localness
@@ -18,74 +13,51 @@ class ProductsController < ApplicationController
     end
     render :index
   end
-  # above is an attempt to implement Travis version
+
+  def new
+    @product = Product.new
+    render :new
+  end
 
 
-  # def localness
-  #   params[:format]
-  #   @products = Product.localness
-  #   render :local
-  # end
-  #
-  #  def three_most_recent
-  #    params[:format]
-  #    @products = Product.three_most_recent
-  #    render :recents
-  #  end
-  #
-  #  def most_reviews
-  #    params[:format]
-  #    @products = Product.most_reviews
-  #    render :most_rev
-  #  end
-
-  #
-
-
-    def new
-      @product = Product.new
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product successfully added!"
+      redirect_to products_path
+    else
       render :new
     end
+  end
 
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
 
-    def create
-      @product = Product.new(product_params)
-      if @product.save
-        flash[:notice] = "Product successfully added!"
-        redirect_to products_path
-      else
-        render :new
-      end
-    end
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
 
-    def edit
-      @product = Product.find(params[:id])
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
       render :edit
     end
-
-    def show
-      @product = Product.find(params[:id])
-      render :show
-    end
-
-    def update
-      @product= Product.find(params[:id])
-      if @product.update(product_params)
-        redirect_to products_path
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @product = Product.find(params[:id])
-      @product.destroy
-      flash[:notice] = "Product deleted!"
-      redirect_to products_path
-    end
-
-    private
-    def product_params
-      params.require(:product).permit(:name, :cost, :country_of_origin)
-    end
   end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = "Product deleted!"
+    redirect_to products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country_of_origin)
+  end
+end
